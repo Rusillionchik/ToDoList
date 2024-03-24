9e120897f19316518f52882fe9bba7edb8d080a7
Rusillionchik <113810196+Rusillionchik@users.noreply.github.com>
2024-03-24 22:37:47 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,30 +6,49 @@ const highBtn = document.querySelector('.addHighTask');
 const lowInput = document.querySelector('.lowInput');
 const lowBtn = document.querySelector('.addLowTask');
 
-const toDo = [];
+let toDo = [];
 
 const createNewDiv = (textContent, classname) => {
     const newDiv = document.createElement('div')
     const insideDiv = document.createElement('div')
+    const newInputDev = document.createElement('div')
     const newCheckbox = document.createElement('input')
     newCheckbox.setAttribute('type', 'checkbox');
     const newBtn = document.createElement('button')
     newBtn.textContent = 'х'
     const textDiv = document.createElement('div')
     textDiv.textContent = textContent
+   
 
 
 
+    
+    newInputDev.appendChild(newBtn)
+    newDiv.appendChild(newInputDev)
     insideDiv.appendChild(newCheckbox)
     newDiv.appendChild(insideDiv)
     newDiv.appendChild(textDiv)
-    newDiv.appendChild(newBtn)
+    
 
-
+    newInputDev.className = 'newInputDev'
     newBtn.className = 'newBtn'
     newCheckbox.className = 'newCheckbox'
     newDiv.className = classname
     textDiv.className = 'textDiv'
+
+    newCheckbox.addEventListener('click', function() {
+    if (this.checked) {
+    textDiv.classList.add('strike-through')
+    }
+    else {
+    textDiv.classList.remove('strike-through')
+    }
+    })
+    newBtn.addEventListener('click', function() {
+    toDo = toDo.filter(item => item !== textContent)
+    newDiv.remove()
+    })
+
     return newDiv
 }
 
@@ -47,10 +66,5 @@ const newValue = (event) => {
 
 form.addEventListener('submit', newValue)
 
-const deleteClick = (event) => {
-    if (event.target.classList.contains('newBtn')) {
-        alert('Удалить див');
-    }
-}
-document.querySelector('.container').addEventListener('click', deleteClick);
+
 
